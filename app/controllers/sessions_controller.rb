13df9847384773c763c988da817edd59c45986b1
ave-hikari <ave.nxson@gmail.com>
2018-05-10 00:28:03 +0900

    ログイン機構完成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      redirect_to user
     else
       # nowをつける
       flash.now[:danger] = 'Invalid email/password combination'
