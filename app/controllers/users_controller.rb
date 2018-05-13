84f0a6b46e432021386f410aa12e0e88ec74a9a6
ave-hikari <ave.nxson@gmail.com>
2018-05-14 00:57:39 +0900

    Create User Edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
