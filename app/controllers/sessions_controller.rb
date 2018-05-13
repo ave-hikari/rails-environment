5fd3e9d7ef4497795a32fca0244a057763f2288a
ave-hikari <ave.nxson@gmail.com>
2018-05-13 12:42:27 +0900

    ログイン修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       redirect_to user
     else
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
