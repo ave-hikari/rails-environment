8a595da6d1c62dd02252ed1422ce87f65dee818c
ave-hikari <ave.nxson@gmail.com>
2018-06-04 00:56:06 +0900

    micropost作成機能、shared/error_messageをusers以外からでも使えるように修正

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,8 +3,21 @@ class MicropostsController < ApplicationController
 
   # ApplicationControllerでログイン確認済み
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
