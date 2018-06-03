45931cab87ea0a08aec051dacc9559e46f640e8e
ave-hikari <ave.nxson@gmail.com>
2018-06-03 22:14:21 +0900

    前に作成したmicropostと紐付け、テスト作成

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ApplicationRecord
   # 単一のmicropostは単一のuserにのみbelongs_toする
   belongs_to :user
+  
+  validates :user_id, presence: true
   # content のバリデートルール 最長140バイト、必須
   validates :content, presence: true, length: { maximum: 140 }
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:hikari)
+    @micropost = @user.microposts.build(content: "test")
+    # 以下と同じ
+    # @micropost = Micropost.new(content: "test", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
