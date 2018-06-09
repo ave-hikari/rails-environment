881ddb9b58dc5275a851f32108fd3414b350b1fc
ave-hikari <ave.nxson@gmail.com>
2018-06-10 00:10:24 +0900

    画像うp時のリサイズとバリデーション設定

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,13 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   # content のバリデートルール 最長140バイト、必須
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+
+  private
+  # サイズチェック
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
